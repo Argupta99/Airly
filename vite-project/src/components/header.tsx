2d3294d3818277cff1218ffe73b0aa039ddb3211
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-11-17 21:00:42 +0530

    dark/light theme

--- vite-project/src/components/header.tsx
+++ vite-project/src/components/header.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../context/theme-provider";
 
 
 const Header = () => {
+
+    const {theme, setTheme} = useTheme();
+    const isDark = theme === "dark";
+    //to toggle between dark and light mode
   return (
-    <header>
-        <div>
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur">
+        <div className="container mx-auto flex h-16 items-center justify-between px-4">
        <Link to={"/"}>
-       <img src="/dark_logo.jpg" className="h-16"/>
+       <img src={isDark ? "/dark_logo.jpg" : "/light_logo.jpg"} className="h-16"/>
        </Link>
+
+       <div>
+        <div onClick={() => setTheme(isDark ? "light" : "dark")}>toggle</div>
+       </div>
        </div>
     </header>
   )
