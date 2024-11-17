d9148672b42ae462ca0055c99b3d8a8ca6576698
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-11-17 21:27:52 +0530

    initializing tanstack query

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,11 +5,15 @@ import Layout from './components/layout'
 import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/weather-dashboard'
 import CityPage from './pages/city-page'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
   
-
+  const queryClient = new QueryClient();
+  //tanstackquery
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <ThemeProvider defaultTheme='dark'>
     <Layout>
@@ -24,6 +28,8 @@ function App() {
     
     
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
